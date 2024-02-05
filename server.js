0844a79732588e8346a8e6f173cced41e8b9c640
leocabs29 <152485105+leocabs29@users.noreply.github.com>
2024-02-05 22:45:18 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let messages = [];
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
+app.post('/messages', (req, res) => {
+  const { user, message } = req.body;
+  if (user && message) {
+    const newMessage = { user, message, timestamp: new Date() };
+    messages.push(newMessage);
+    res.json(newMessage);
+  } else {
+    res.status(400).send('Invalid request');
+  }
+});
+
+app.delete('/clear-messages', (req, res) => {
+    // Clear the messages array
+    messages = [];
+    res.send('Messages cleared');
+});
+
+
+// The catch-all route should be placed at the end
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
