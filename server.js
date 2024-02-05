eb23979f67941f1df99ba55bd2abecedc7fac244
leocabs29 <152485105+leocabs29@users.noreply.github.com>
2024-02-05 23:12:03 +0800

    asdd

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -39,6 +39,9 @@ app.get('*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
